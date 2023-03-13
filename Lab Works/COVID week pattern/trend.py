6f3e96a9ae09310ed0fbb70417802395bb285518
luminaa <rahual.rai@bison.howard.edu>
2023-03-13 00:52:16 -0400

    added daily percentages

--- Lab Works/COVID week pattern/trend.py
+++ Lab Works/COVID week pattern/trend.py
@@ -1,3 +1,7 @@
+import csv
+
+
+
 # Get chunks from a file
 def get_chunks_from_file(file_path):
     chunks = []
@@ -83,6 +87,41 @@ def get_mentions_with_cough_cold_fever(dic):
 
     return cough_mentions, cold_mentions, fever_mentions
 
+
+# Get daily percentage of posts that mention cough, cold, and fever
+def get_daily_mention_percentages(daily_posts, daily_cough_posts, daily_cold_posts, daily_fever_posts):
+    daily_cough_percentage = []
+    daily_cold_percentage = []
+    daily_fever_percentage = []
+
+    for i in range(len(daily_posts)):
+        daily_cough_percentage.append(round(daily_cough_posts[i] / daily_posts[i] * 100, 3))
+        daily_cold_percentage.append(round(daily_cold_posts[i] / daily_posts[i] * 100, 3))
+        daily_fever_percentage.append(round(daily_fever_posts[i] / daily_posts[i] * 100, 3))
+
+    return daily_cough_percentage, daily_cold_percentage, daily_fever_percentage
+
+
+def write_daily_outputs_to_csv(daily_posts, daily_cough_posts, daily_cold_posts, daily_fever_posts, daily_cough_percentage, daily_cold_percentage, daily_fever_percentage):
+    with open('daily_outputs.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+
+        # Write the header row with the days
+        writer.writerow(['', 'April 12', 'April 13', 'April 14', 'April 15', 'April 16', 'April 17', 'April 18'])
+
+        # Write the daily counts for posts and mentions
+        writer.writerow(['Total Posts'] + daily_posts)
+        writer.writerow(['Cough Mentions'] + daily_cough_posts)
+        writer.writerow(['Cold Mentions'] + daily_cold_posts)
+        writer.writerow(['Fever Mentions'] + daily_fever_posts)
+
+        # Write the daily percentages for posts that mention cough, cold, and fever
+        writer.writerow(['Cough Percentage'] + daily_cough_percentage)
+        writer.writerow(['Cold Percentage'] + daily_cold_percentage)
+        writer.writerow(['Fever Percentage'] + daily_fever_percentage)
+
+
+
 base_path = "daily_discussion_april_"
 file_extension = ".txt"
 
@@ -91,18 +130,18 @@ daily_cough_posts = []
 daily_cold_posts = []
 daily_fever_posts = []
 
-for day in range(12,19):
+# Loop through the days and get the daily posts and mentions
+for day in range(12, 19):
     file_path = base_path + str(day) + file_extension
     chunks = get_chunks_from_file(file_path)
     dic, users, users_with_multiple_posts = get_users_comments_from_chunks(chunks)
     cough_mentions, cold_mentions, fever_mentions = get_mentions_with_cough_cold_fever(dic)
 
+    # Append the daily counts to the corresponding lists
     daily_posts.append(len(chunks))
     daily_cough_posts.append(len(cough_mentions))
     daily_cold_posts.append(len(cold_mentions))
     daily_fever_posts.append(len(fever_mentions))
 
-print(daily_posts)
-print(daily_cough_posts)
-print(daily_cold_posts)
-print(daily_fever_posts)
+daily_cough_percentage, daily_cold_percentage, daily_fever_percentage = get_daily_mention_percentages(daily_posts, daily_cough_posts, daily_cold_posts, daily_fever_posts)
+write_daily_outputs_to_csv(daily_posts, daily_cough_posts, daily_cold_posts, daily_fever_posts, daily_cough_percentage, daily_cold_percentage, daily_fever_percentage)
