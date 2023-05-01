d50dc4ff2cdb47f542d44eb57a036dcd2d70bc6a
luminaa <rairahual7@gmail.com>
2023-04-30 20:00:10 -0400

    added table of contents

--- Class Works/Data Structures/Amtrak.py
+++ Class Works/Data Structures/Amtrak.py
@@ -11,8 +11,8 @@ class LinkedList:
             for item in lst:
                 self.append(item)
 
-    def append(self, data):
-        new_node = Node(data) # create a new node with the given data
+    def append(self, data): # adding a new node at the end of the list
+        new_node = Node(data) # creating a new node with the given data
         if self.head is None: # if the list is empty, make the new node the head
             self.head = new_node
             self.tail = new_node
@@ -20,7 +20,7 @@ class LinkedList:
             self.tail.next = new_node
             self.tail = new_node 
 
-    def insert(self, data, position, head=None):
+    def insert(self, data, position, head=None): # adding a new node at the given position
         if head is None:
             head = self.head
         if position == 0:
@@ -31,7 +31,7 @@ class LinkedList:
             head.next = self.insert(data, position-1, head.next)
         return head
     
-    def insert_after(self, data, after, head=None):
+    def insert_after(self, data, after, head=None): # adding a new node after the node with the given data
         if head is None:
             head = self.head
         if head.data == after:
@@ -42,7 +42,7 @@ class LinkedList:
             head.next = self.insert_after(data, after, head.next)
         return head
     
-    def remove(self, position, head=None):
+    def remove(self, position, head=None): # removing a node from the given position
         if head is None:
             head = self.head
         if position == 0:
@@ -62,10 +62,10 @@ class LinkedList:
             string += ' → '
 
 amtrak_stations = ['Union Station', 'New Carrollton', 'BWI', 'Baltimore', 'Wilmington Delaware', 'Philadelphia']
-amtrak = LinkedList(amtrak_stations)
-print(amtrak)
+amtrak = LinkedList(amtrak_stations) # create a linked list from the list of stations
+print(amtrak) 
 amtrak.insert_after('Aberdeen', 'Baltimore') # insert Aberdeen after Baltimore
 # amtrak.insert('Aberdeen', 4) # insert Aberdeen at position 4 (after Baltimore)
 print(amtrak)
-amtrak.remove(4) # remove Aberdeen
+amtrak.remove(4) # remove the node ('Aberdeen') at position 4
 print(amtrak)
--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # CSCI-135 (Computer Science 1) Repository
-This repository contains my work from my Computer Science 1 course. All of the code is written in Python.
+This repository contains my work from my Computer Science 1 course at Howard Uniuversity. All of the code is written in Python.
 
 ## Usage
-To use the code in this repository, simply clone the repository to your local machine and run the code using a Python interpreter. You can also view the code online by visiting the repository on Github.
+To use the code in this repository, Clone the repository to your local machine using Git:
+```bash
+git clone https://github.com/luminaa/CSCI-135
+```
+
+## Table of Contents
+[Project 1: Introduction to Objects](/Class Works/Introductions to Objects)
+Project 2: Covid File Data Analysis
+Project 3: Data Structures
+Final Project: Amtrak Stations
+
+
 
 ## Contributing
 If you find any errors or would like to suggest improvements, please feel free to create a pull request or open an issue. Your feedback is greatly appreciated!
